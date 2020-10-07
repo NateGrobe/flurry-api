39b37f2c48af1f2e081debf2f4b590846c038ffb
Nate Grobe <natepgrobe@gmail.com>
2020-10-07 17:47:22 -0400

    probs shoulda left build in the repo for heroku...

---
+++ build/precache-manifest.1759b6b557badf8c0c59f56885231031.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f46e383ddcad6c2f9c2d677a494a1494",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68ec30fd571eded8cca5",
+    "url": "/static/css/main.d8bcff04.chunk.css"
+  },
+  {
+    "revision": "a50d6ed5a23405f15674",
+    "url": "/static/js/2.9d73d1c9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9d73d1c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68ec30fd571eded8cca5",
+    "url": "/static/js/main.d7f77c15.chunk.js"
+  },
+  {
+    "revision": "f28a8765af3946df17b5",
+    "url": "/static/js/runtime-main.862f3953.js"
+  }
+]);
